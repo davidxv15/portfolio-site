428a5b929f08e7b59708b7b8952b3aa806a2c6c9
davidxv15 <davidxvaz@gmail.com>
2024-11-11 23:06:54 -0700

    App.tsx start

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className="text-center p-8 bg-blue-500 text-white">
-      <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
-      <p>This text should be styled with Tailwind.</p>
+      <h1 className="text-4xl font-bold">Portfolio</h1>
+      <p>Styles</p>
     </div>
   );
 };
