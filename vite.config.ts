bf7f99da8b8486b9e7de7f4a0aea7bf0ec89e2d7
davidxv15 <davidxvaz@gmail.com>
2024-11-12 22:52:18 -0700

    vite.config notes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,  // Optionally open the browser automatically
+    open: true,  // open browser automatically
   },
 });
