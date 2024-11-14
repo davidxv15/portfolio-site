e9295b87b2e585575dec8fe271e78b8dfd4a4f68
davidxv15 <davidxvaz@gmail.com>
2024-11-13 23:17:44 -0700

    HeroSection to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className="text-center p-8 bg-blue-500 text-white">
-      <h1 className="text-4xl font-bold">Portfolio</h1>
-      <p>Styles</p>
+      <HeroSection />
     </div>
   );
 };
