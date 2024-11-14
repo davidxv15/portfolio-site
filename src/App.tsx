6a3c8f3e9cdd1acaa24d24cb5be9353985af3317
davidxv15 <davidxvaz@gmail.com>
2024-11-14 09:51:13 -0700

    render

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HeroSection from './HeroSection';
 const App: React.FC = () => {
   return (
     <div className="text-center p-8 bg-blue-500 text-white">
+      <h1>ing</h1>
       <HeroSection />
     </div>
   );
